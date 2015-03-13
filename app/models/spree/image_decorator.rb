90e0f9e9b4cca00c922a8718331187b5b50cadb7
cuizheng <zheng.cuizh@gmail.com>
2015-03-14 00:07:20 +0800

    为了避免中文名称问题，需要把附件名称重写

--- app/models/spree/image_decorator.rb
+++ app/models/spree/image_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Image.class_eval do
-    
+
     has_attached_file   :attachment,
                         :storage => :aliyun,
                         styles: { mini: '48x48>', small: '100x100>', product: '240x240>', large: '600x600>' },
@@ -8,4 +8,12 @@ Spree::Image.class_eval do
                         path: ':rails_root/public/spree/products/:id/:style/:basename.:extension',
                         convert_options: { all: '-strip -auto-orient -colorspace sRGB' }
 
+    before_create :randomize_file_name
+
+    private
+
+    def randomize_file_name
+        extension = File.extname(attachment_file_name).downcase
+        self.attachment.instance_write(:file_name, "#{SecureRandom.hex(16)}#{extension}")
+    end
 end
